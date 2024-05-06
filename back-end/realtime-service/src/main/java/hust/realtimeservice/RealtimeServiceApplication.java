ddc7d60f4962c2f8d9184722530fa97564f81ea6
taquanglinh <tqlinh20194605@gmail.com>
2024-05-06 21:26:09 +0700

    Project Initialize

---
+++ back-end/realtime-service/src/main/java/hust/realtimeservice/RealtimeServiceApplication.java
@@ -0,0 +1,13 @@
+package hust.realtimeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeServiceApplication.class, args);
+	}
+
+}
---
+++ back-end/realtime-service/src/test/java/hust/realtimeservice/RealtimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hust.realtimeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
